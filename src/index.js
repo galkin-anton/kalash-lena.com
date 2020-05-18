95aa312c5ff96c366e9572f32489dc6ee4c420db
Galkin Anton <galkin88@gmail.com>
2020-05-18 11:47:59 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class HelloWorld extends React.Component {
+    render() {
+        return (
+                <h1 {...this.props}>
+                    Hello {this.props.frameworkName} World
+                </h1>
+        )
+    }
+}
+
+class DateTimeNow extends React.Component {
+    render() {
+        return <h1>Date time
+            Now {new Date().toLocaleDateString()} {new Date().toLocaleTimeString()}
+        </h1>
+    }
+}
+
+/*
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+        React.createElement('div',
+                null,
+                React.createElement(HelloWorld, {
+                    id: "ember",
+                    frameworkname: "Ember.js",
+                    title: "bla bla bla 1"
+                }),
+                React.createElement(HelloWorld, {
+                    id: "backbone",
+                    frameworkname: "Backbone.js",
+                    title: "bla bla bla 2"
+                }),
+                React.createElement('a', {href: 'http://node.university'},
+                        'some'),
+                React.createElement(HelloWorld, {
+                    id: "angular",
+                    frameworkname: "Angular.js",
+                    title: "bla bla bla 3"
+                })
+        ),
+        document.getElementById('root')
 );
+*/
+
+ReactDOM.render(
+        <div>
+            <HelloWorld
+                    id='ember'
+                    frameworkName='Ember.js'
+                    title='bla bla bla 1'/>
+            <HelloWorld
+                    id='backbone'
+                    frameworkName='Backbone.js'
+                    title='bla bla bla 2'/>
+            <HelloWorld
+                    id='angular'
+                    frameworkName='Angular.js'
+                    title='bla bla bla 3'/>
+            <DateTimeNow/>
+        </div>,
+        document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
